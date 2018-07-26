0ae598d21cddafed97e8ac5c7766e4c4c6437b38
AlexS <alexsuvorov94@mail.ru>
2018-07-26 20:32:16 +0400

    Adapter is added, json was processed

--- app/src/main/java/ru/alexsuvorov/employeeslist/SpecialtyListActivity.java
+++ app/src/main/java/ru/alexsuvorov/employeeslist/SpecialtyListActivity.java
@@ -5,7 +5,6 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -27,13 +26,13 @@ import ru.alexsuvorov.employeeslist.utils.HttpHandler;
 import ru.alexsuvorov.employeeslist.utils.Utils;
 
 public class SpecialtyListActivity extends AppCompatActivity {
-
     private static final String URL = "http://gitlab.65apps.com/65gb/static/raw/master/testTask.json";
-    public static ArrayList<Specialty> specialtyList = new ArrayList<>();
-    public static ArrayList<Worker> workerList = new ArrayList<>();
+    public ArrayList<Specialty> specialtyList = new ArrayList<>();
+    public ArrayList<Worker> workerList = new ArrayList<>();
     ListView listView;
     EmployeesListFragment employeesListFragment;
-    FragmentTransaction fTrans;
+    private final String TAG = getClass().getSimpleName();
+    DataLoader loader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +41,11 @@ public class SpecialtyListActivity extends AppCompatActivity {
         listView = findViewById(R.id.spec_list);
         employeesListFragment = new EmployeesListFragment();
         if (Utils.isNetworkAvailable(this)) {
-            new DataLoader(this).execute(URL);
-            //new loader.execute(URL);
+            loader = new DataLoader(this);
+            if (!loader.isCancelled()) {
+                loader.execute(URL);
+            }
+            //new DataLoader(this).execute(URL);
         } else {
             Toast.makeText(this, "No Network Connection", Toast.LENGTH_LONG).show();
         }
@@ -68,6 +70,49 @@ public class SpecialtyListActivity extends AppCompatActivity {
         }
 
         @Override
+        protected Void doInBackground(String... params) {
+            if (!isCancelled()) {
+                try {
+                    HttpHandler sh = new HttpHandler();
+                    String jsonStr = sh.makeServiceCall(params[0]);
+                    try {
+                        JSONObject reader = new JSONObject(jsonStr);
+                        JSONArray response = reader.getJSONArray("response");
+                        for (int i = 0; i < response.length(); i++) {
+                            String workerFName = response.getJSONObject(i).getString("f_name");
+                            String workerLName = response.getJSONObject(i).getString("l_name");
+                            String workerBithday = response.getJSONObject(i).getString("birthday");
+                            String workerAvatarUrl = response.getJSONObject(i).getString("avatr_url");
+                            int workerSpecialtyId = response.getJSONObject(i)
+                                    .getJSONArray("specialty")
+                                    .getJSONObject(0)
+                                    .getInt("specialty_id");
+                            String specialtyName = response.getJSONObject(i)
+                                    .getJSONArray("specialty")
+                                    .getJSONObject(0)
+                                    .getString("name");
+                            Specialty specialty = new Specialty(workerSpecialtyId, specialtyName);
+                            if (!specialtyList.contains(specialty)) {
+                                specialtyList.add(specialty);
+                            }
+                            Worker worker = new Worker(workerFName, workerLName);
+                            worker.setBithday(workerBithday);
+                            worker.setAvatarLink(workerAvatarUrl);
+                            worker.setSpecialty(workerSpecialtyId);
+                            workerList.add(worker);
+                        }
+                        Log.d(TAG, workerList.get(2).getF_name());
+                    } catch (final JSONException e) {
+                        Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
             if (mContext != null) {
@@ -78,63 +123,9 @@ public class SpecialtyListActivity extends AppCompatActivity {
                 listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                        /*Intent i = new Intent(More.this, NextActvity.class);
-                        //If you wanna send any data to nextActicity.class you can use
-                        i.putExtra(String key, value.get(position));
-
-                        startActivity(i);*/
                     }
                 });
             }
-        }
-
-        @Override
-        protected Void doInBackground(String... params) {
-            try {
-                HttpHandler sh = new HttpHandler();
-                String jsonStr = sh.makeServiceCall(params[0]);
-                try {
-                    JSONObject reader = new JSONObject(jsonStr);
-                    JSONArray response = reader.getJSONArray("response");
-
-                    for (int i = 0; i < response.length(); i++) {
-                        String workerFName = response.getJSONObject(i).getString("f_name");
-                        //Log.d(TAG, workerFName);
-                        String workerLName = response.getJSONObject(i).getString("l_name");
-                        //Log.d(TAG, workerLName);
-                        String workerBithday = response.getJSONObject(i).getString("birthday");
-                        //Log.d(TAG, workerBithday);
-                        String workerAvatarUrl = response.getJSONObject(i).getString("avatr_url");
-                        //Log.d(TAG, workerAvatarUrl);
-                        int workerSpecialtyId = response.getJSONObject(i)
-                                .getJSONArray("specialty")
-                                .getJSONObject(0)
-                                .getInt("specialty_id");
-                        Log.d(TAG, "ID: " + workerSpecialtyId);
-                        String specialtyName = response.getJSONObject(i)
-                                .getJSONArray("specialty")
-                                .getJSONObject(0)
-                                .getString("name");
-                        Log.d(TAG, "Name: " + specialtyName);
-                        Specialty specialty = new Specialty(workerSpecialtyId, specialtyName);
-                        if (!specialtyList.contains(specialty)) {
-                            specialtyList.add(specialty);
-                        }
-                        Worker worker = new Worker(workerFName, workerLName);
-                        worker.setBithday(workerBithday);
-                        worker.setAvatarLink(workerAvatarUrl);
-                        worker.setSpecialty(workerSpecialtyId);
-                        workerList.add(worker);
-
-                    }
-                    //Log.d(TAG, "SPECIALTY: "+specialtyList.size());
-                } catch (final JSONException e) {
-                    Log.e(TAG, "Json parsing error: " + e.getMessage());
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
         }
     }
 }
